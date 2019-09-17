f563f72137de27056a523fad7a60efad5091f09a
Oleh <eyeofatiger@yandex.ua>
2019-09-17 11:43:18 +0100

    Finish

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 let inputUsd = document.getElementById("usd"),
     inputGbp = document.getElementById("gbp");
 
-    
+    inputUsd.addEventListener("input", ()=>{
+        let request = new XMLHttpRequest();
+
+        request.open("GET", "current.json");
+        request.setRequestHeader("Content-type", "aplication/json; charset=utf-8");
+        request.send();
+
+        request.addEventListener("readystatechange", function(){
+            if(request.readyState ===4 && request.status ==200){
+                let data = JSON.parse(request.response);
+                
+                inputGbp.value = (inputUsd.value / data.gbp).toFixed(2);
+            } else{
+                inputGbp.value = "Somthing wrong!";
+            }
+        });
+    });
